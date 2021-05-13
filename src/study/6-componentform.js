b7ce753f0fbcfa316e5745f86c02718eb519abb5
Mrshihe <1431516920@qq.com>
2021-05-13 14:42:11 +0800

    add

--- src/study/6-componentform.js
+++ src/study/6-componentform.js
@@ -4,13 +4,28 @@ import React from 'react';
 1. 实时验证 2.有条件的禁用按钮 3.强制输入指定格式
 */
 class Controlled extends React.Component {
+  // state成为表单数据源
+  state = {
+    username: '',
+    password: ''
+  }
+  saveData = e => {
+    const name = e.target.name
+    this.setState({
+      [name]: e.target.value
+    })
+  }
+  controlledSumbit = e => {
+    e.preventDefault()
+    console.log( this.state )
+  }
   render(){
     return (
       <div>
         <h1>受控组件</h1>
-        <form>
-          <input type="text" placeholder="请输入用户名" /><br />
-          <input type="password" placeholder="请输入密码" /><br />
+        <form onSubmit={ this.controlledSumbit }>
+          <input type="text" name="username" placeholder="请输入用户名" onChange={ this.saveData } /><br />
+          <input type="password" name="password" placeholder="请输入密码" onChange={ this.saveData } /><br />
           <button>登录</button>
         </form>
       </div>
